5aa7edef63fd981c43db95d019221d512cbfc414
betheredge <benjamin.etheredge@gmail.com>
2016-05-03 22:41:41 -0500

    Began basic backend

---
+++ Sudoku/Sudoku/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    class Constants
+    {
+        easyPuzzle =       {0.0.0,2,6,0,7,0,1}, 
+        SolvedEasyPuzzle = {4,3,5,2,6,9,7,8,1}, {6,8,2,5,7,1,4,9,3}, {
+        
+    }
+}
---
+++ Sudoku/Sudoku/Sudoku.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    class Sudoku
+    {
+        public const int BOARD_SIZE = 9;
+
+
+
+        public int[,] solutionBoard;
+        public int[,] playerBoard;
+
+
+        public Sudoku ()
+        {
+
+            this.playerBoard = new int[BOARD_SIZE, BOARD_SIZE];
+
+
+
+        }
+
+
+
+
+
+    }
+
+    class Constants
+    {
+        
+    }
+}
